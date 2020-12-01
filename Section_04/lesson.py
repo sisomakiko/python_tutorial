e99073c5a2d67a5d6eddf6b0f2f8884b29dfd310
sisomakiko <siso.ndmzbr.mash@gmail.com>
2020-12-01 22:03:36 +0900

    辞書型についてのレッスンを受講

--- Section_04/lesson.py
+++ Section_04/lesson.py
@@ -1,31 +1,19 @@
-num_tuple = (10, 20)
-print(num_tuple)
+x = {'a': 1}
+y = x
+y['a'] = 1000
+print(x)
+print(y)
 
-x, y = num_tuple
-print(x, y)
+x = {'a': 1}
+y = x.copy()
+y['a'] = 1000
+print(x)
+print(y)
 
-x, y = (10, 20)
-print(x, y)
+fruits = {
+    'apple': 100,
+    'banana': 200,
+    'orange': 300
+}
 
-i = 10
-j = 20
-tmp = i
-i = j
-j = tmp
-print(i, j)
-
-a = 100
-b = 200
-print(a, b)
-a, b = b, a
-print(a, b)
-
-print('----------------')
-
-choose_from_two = ('A', 'B', 'C')
-answer = []
-answer.append('A')
-answer.append('C')
-
-print(choose_from_two)
-print(answer)
+print('price of apple is {}'.format(fruits['apple']))
